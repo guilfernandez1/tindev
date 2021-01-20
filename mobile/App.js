b16a4276491bf189bcdb6dc084d8de8f9d5fd5c0
Guilherme Lameiras Fernandez <guil.fernandez1@hotmail.com>
2021-01-20 00:11:22 -0300

    Mobile: Inicio da aplicação mobile, estruturação de pastas

---
+++ mobile/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    justifyContent: 'center'
+  },
+});

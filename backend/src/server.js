4ec06b5cd5e26b2215f9f7e3405aa0b0dd657e8d
Guilherme Lameiras Fernandez <guil.fernandez1@hotmail.com>
2021-01-22 16:21:33 -0300

    Front-end: Finalização de efeito de match.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
 const routes = require('./routes');
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+    connectedUsers[user] = socket.id;
+});
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0.xsyzh.mongodb.net/<dbname>?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
